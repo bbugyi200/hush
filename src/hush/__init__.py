b89cc3b10bfed89b497f0ae8b71625a8f0f3a675
Bryan Bugyi <bryanbugyi34@gmail.com>
2021-10-04 21:47:25 -0400

    Bump version: 0.1.0 → 0.2.0

--- src/hush/__init__.py
+++ src/hush/__init__.py
@@ -11,6 +11,6 @@ from .hush import get_secret
 
 __author__ = "Bryan M Bugyi"
 __email__ = "bryanbugyi34@gmail.com"
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 _logging.getLogger(__name__).addHandler(_logging.NullHandler())
