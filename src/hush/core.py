79d5565e6141eb97c23e353573afcd5515a0230c
Bryan Bugyi <bryanbugyi34@gmail.com>
2021-10-04 22:32:24 -0400

    Improve get_secret() docstring

--- src/hush/core.py
+++ src/hush/core.py
@@ -13,12 +13,22 @@ from . import plugin
 def get_secret(key: str, namespace: Iterable[str] = tuple()) -> Optional[str]:
     """Given a ``key``, retrieve a secret.
 
+    This function attempts to use every secret-retrieving method registered by
+    plugins (internal and external) to obtain the desired secret.
+
     Args:
         key: The key that corresponds to the secret we are hoping to retrieve.
         namespace: The namespace that the secret belongs to (e.g. `["db",
           "foobar"]`). How this argument is used is specific to the tool being
           used to store and retrieve secrets (i.e. is specific to each hook
           implementation).
+
+    Returns:
+        The secret value returned by the first plugin to successfully retrieve
+        the desired secret.
+            OR
+        None, if none of the registered plugins were able to retrieve the
+        desired secret.
     """
     pm = plugin.manager()
     secret: Optional[str] = pm.hook.get_secret(key=key, namespace=namespace)
