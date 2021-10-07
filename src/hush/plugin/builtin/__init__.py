1f0c74c7a3cd28a8f752ec875127632157d635e5
Bryan Bugyi <bryanbugyi34@gmail.com>
2021-10-07 10:26:46 -0400

    Improve docs for builtin/__init__.py

--- src/hush/plugin/builtin/__init__.py
+++ src/hush/plugin/builtin/__init__.py
@@ -1,10 +1,12 @@
 """Internal (i.e. builtin to hush) plugin hook implementations live here.
 
-All plugin hook implementations[1] found in this module will be enabled by
+All plugin hook implementations[1] found in this package will be enabled by
 default.
 
 [1]: https://pluggy.readthedocs.io/en/stable/#implementations
 """
 
+# These hooks must be imported into this package's global namespace so this
+# package can be registered as a plugin via `PluginManager.register(builtins)`.
 from .envvars import get_secret as _envvars_get_secret
 from .pass_store import get_secret as _pass_store_get_secret
